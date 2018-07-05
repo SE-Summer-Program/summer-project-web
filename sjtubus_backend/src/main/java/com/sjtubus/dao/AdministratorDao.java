f15ded8c5490d722a8d2162b22890216014291f9
EsdeathYZH <1162526220@qq.com>
2018-07-05 17:03:29 +0800

    合并前端的页面，添加管理员

--- sjtubus_backend/src/main/java/com/sjtubus/dao/AdministratorDao.java
+++ sjtubus_backend/src/main/java/com/sjtubus/dao/AdministratorDao.java
@@ -4,4 +4,5 @@ import com.sjtubus.entity.Administrator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdministratorDao extends JpaRepository<Administrator,String> {
+
 }
--- sjtubus_backend/src/main/java/com/sjtubus/entity/Administrator.java
+++ sjtubus_backend/src/main/java/com/sjtubus/entity/Administrator.java
@@ -3,12 +3,14 @@ package com.sjtubus.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Administrator")
 @Data
 public class Administrator {
+    @Id
     String id ;
     String a_username;
     String a_password;
