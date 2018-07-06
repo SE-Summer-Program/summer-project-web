6ea47ed87bf1d4b2191d8a2c4d84955aad767bc5
studentliying <540906886@qq.com>
2018-07-06 17:22:35 +0800

    realize getting shift info operation

--- sjtubus_backend/src/main/java/com/sjtubus/service/AdministratorService.java
+++ sjtubus_backend/src/main/java/com/sjtubus/service/AdministratorService.java
@@ -2,7 +2,9 @@ package com.sjtubus.service;
 
 import com.sjtubus.dao.AdministratorDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdministratorService {
     @Autowired
     private AdministratorDao administratorDao;
--- sjtubus_backend/src/main/java/com/sjtubus/service/RideBusInfoService.java
+++ sjtubus_backend/src/main/java/com/sjtubus/service/RideBusInfoService.java
@@ -2,7 +2,9 @@ package com.sjtubus.service;
 
 import com.sjtubus.dao.RideBusInfoDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RideBusInfoService {
     @Autowired
     RideBusInfoDao rideBusInfoDao;
