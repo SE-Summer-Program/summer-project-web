bbb7ec1f0a0ab10a819bffa9665162ee17c9b36c
ricardo0228 <38351207+ricardo0228@users.noreply.github.com>
2018-07-10 16:59:44 +0800

    查询时刻表

--- sjtubus_backend/src/main/java/com/sjtubus/service/TimeTableService.java
+++ sjtubus_backend/src/main/java/com/sjtubus/service/TimeTableService.java
@@ -2,7 +2,10 @@ package com.sjtubus.service;
 
 import com.sjtubus.dao.TimeTableDao;
 import com.sjtubus.entity.TimeTable;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +15,11 @@ public class TimeTableService {
     @Autowired
     private TimeTableDao timeTableDao;
 
-    public TimeTable getTimeTableByLineName(String station, String type) {
-    	return timeTableDao.findByStationAndType(station, type);
+    public Map<String, List<String>> getScheduleByStationAndType(String station, String type) {
+    	TimeTable timeTable = timeTableDao.findByStationAndType(station, type);
+    	Map<String, List<String>> schedule = new HashMap<String, List<String>>();
+    	schedule.put("loopline", timeTable.getLoopline());
+    	schedule.put("nonloopline", timeTable.getNonloopline());
+    	return schedule;
     }
 }
