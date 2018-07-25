9ecc341467365817855544e1a4a4aca698fb86f8
wangxinwei516030910041 <1351232837@qq.com>
2018-07-25 16:36:42 +0800

    添加取消预约（未完成）

--- sjtubus_backend/src/main/java/com/sjtubus/dao/UserDao.java
+++ sjtubus_backend/src/main/java/com/sjtubus/dao/UserDao.java
@@ -23,7 +23,6 @@ public interface UserDao extends JpaRepository<User,Integer> {
 
     User findByUserId(int userId);
 
-
     @Transactional
     @Modifying
     @Query("update User user set user.username=:username, user.phone=:phone, user.credit=:credit where user.userId =:userId")
